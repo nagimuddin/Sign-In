2a9bb3e3483076f884cb6c5829be2d7f487f312a
nagimuddin520 <nagimuddin520@gmail.com>
2022-07-30 17:58:23 +0600

    first commit

---
+++ src/Shared/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className='h-20 bg-slate-700 text-white flex items-center'>
+        <p className='italic hover:not-italic text-2xl'>SignIn</p>
+    </nav>
+  );
+}
+
+export default Header;
